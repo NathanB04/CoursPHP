f67b0e690b2965d2df3419eb389ad9509c608e79
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-01-25 14:50:38 +0100

    Structures de contrôle if/else

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$valeur = "Michel";
+$resultat= " ";
+
+if (is_numeric($valeur)) {
+if ($valeur > 0) {
+    $resultat = "positive";
+} elseif ($valeur < 0) {
+    $resultat = "negative";
+}
+else {
+    $resultat = "nulle";
+}
+}
+else { 
+    $resultat = "non numerique";
+}
+
+echo "La valeur est $resultat.";
